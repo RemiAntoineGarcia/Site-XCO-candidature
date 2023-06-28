d8800bbd10d0acbec4299a5d35e7edc9d0af84fe
RemiAntoineGarcia <79006460+RemiAntoineGarcia@users.noreply.github.com>
2023-06-28 19:03:38 +0200

    Initiation
    Initialisation du projet

---
+++ Site CXO candidature/Site CXO candidature/IBackEndService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 
+/// </summary>
+namespace Site_CXO_candidature
+{
+    internal interface IBackEndService
+    {
+        IReservation CreateReservation(List<IOrderLine> order);
+        List<I>
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    class Reservation
+    {
+        string ReservationId { get; }
+        DateTime CreaetAt { get; }
+        List<OrderLine> OrdersLines { get; }
+        bool IsAvailable { get; }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    class Inventory
+    {
+        string ProductId { get; }
+        int Quantity { get; set; }
+    }
+
+    ///
+    class OrderLine
+    {
+        string ProductId { get; }
+        int Quantity { get; set; }
+    }
+
+
+
+}
